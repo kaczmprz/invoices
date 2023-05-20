5592aeb3f6f92a1474a93832376bf0c2dea39460
kaczmprz <kaczmprz@gmail.com>
2023-05-20 11:53:36 +0200

    Added changes in app.views.py

--- invoices_app/app/urls.py
+++ invoices_app/app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('customer', views.customer, name='customer'),
+    path('material', views.material, name='material'),
+    path('company', views.company, name='company'),
+    path('invoice', views.invoice, name='invoice'),
 ]
--- invoices_app/app/views.py
+++ invoices_app/app/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from .models import Customer, Material, Company
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'site': 'home'
+    }
+    return render(request, 'index.html', context)
+
+def customer(request):
+    customer_list = Customer.objects.order_by("-last_name")[:5]
+    context = {
+        'site': 'customer',
+        'list': customer_list
+    }
+    return render(request, 'index.html', context)
+
+def material(request):
+    context = {
+        'site': 'material'
+    }
+    return render(request, 'index.html', context)
+
+def company(request):
+    context = {
+        'site': 'company'
+    }
+    return render(request, 'index.html', context)
+
+def invoice(request):
+    context = {
+        'site': 'invoice'
+    }
+    return render(request, 'index.html', context)
